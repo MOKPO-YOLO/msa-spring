57120b32ab8b6ab23dafb1cf6a548727a089f6ae
oh jinhee <exojj159@gmail.com>
2024-06-05 10:30:03 +0900

    spring -jini 작업

--- src/main/java/com/smhrd/controller/BoardRestController.java
+++ src/main/java/com/smhrd/controller/BoardRestController.java
@@ -21,6 +21,7 @@ import com.smhrd.entity.Detection;
 import com.smhrd.entity.Member;
 import com.smhrd.entity.MonthlySum;
 import com.smhrd.entity.Notice;
+import com.smhrd.entity.TodayYN;
 import com.smhrd.entity.Workload;
 import com.smhrd.mapper.BoardMapper;
 
@@ -31,67 +32,78 @@ import com.smhrd.mapper.BoardMapper;
 @RestController
 @CrossOrigin // CorsFiler class생성해두어서 이제 originPatterns 이거 필요 없다. @CrossOrigin(originPatterns = "http://localhost:3000") -> @CrossOrigin("http://localhost:3000")
 public class BoardRestController {
-		// 실험으로 구현한 가상의 DB 회원정보 Member ArrayList<>
-		// List<Member> members = new ArrayList<>();
-		
+      // 실험으로 구현한 가상의 DB 회원정보 Member ArrayList<>
+      // List<Member> members = new ArrayList<>();
+      
 
-		@Autowired
-		private BoardMapper mapper;
-	
-		// 공지사항 전체보기
-		@GetMapping("/all") //  /notice/all
-		public List<Notice> noticeList(){
-			System.out.println("[공지사항 전체보기]");
-			
-			List<Notice> noticeList = mapper.noticeList();
-			System.out.println(noticeList);
-			return noticeList;
-		}
-		
-		
-		// 위해물품 월별합산 전체 작업량보기
-		@GetMapping("/workloadall")  //  /notice/workloadall
-		public List<MonthlySum> workloadList(){
-			System.out.println("[위해물품 작업량 전체보기]");
-			
-			List<MonthlySum> workloadList = mapper.workloadList();
-			System.out.println(workloadList);
-			
-			return workloadList;
-		}
-		
-		
-		
-		
-		
-		
-	
-		
-		
-		// 게시글 상세보기
-//		@GetMapping("/{idx}") // board/{idx}
-//		public Board boardContent(@PathVariable("idx") int idx) {
-//			Board board = mapper.boardContent(idx);
-//			return board;
-//		}
+      @Autowired
+      private BoardMapper mapper;
+   
+      // 공지사항 전체보기
+      @GetMapping("/all") //  /notice/all
+      public List<Notice> noticeList(){
+         System.out.println("[공지사항 전체보기]");
+         
+         List<Notice> noticeList = mapper.noticeList();
+         System.out.println(noticeList);
+         return noticeList;
+      }
+      
+      
+      // 위해물품 월별합산 전체 작업량보기
+      @GetMapping("/workloadall")  //  /notice/workloadall
+      public List<MonthlySum> workloadList(){
+         System.out.println("[위해물품 작업량 전체보기]");
+         
+         List<MonthlySum> workloadList = mapper.workloadList();
+         System.out.println(workloadList);
+         
+         return workloadList;
+      }
+      
+      
+      // 금일 날짜의 탐지 Y갯수 호출
+      @GetMapping("/gettodayYN")  //  /notice/gettodayYN
+      public TodayYN gettodayYN(){   
+         TodayYN today = mapper.getToday();
+         System.out.println(today.getTODAY()); //먼저 금일(오늘) 날짜 가져오는 쿼리 호출 해줌.
+         
+         today.setTODAY_Y(mapper.gettodayY(today.getTODAY()).getTODAY_Y());
+         today.setTODAY_N(mapper.gettodayN(today.getTODAY()).getTODAY_N());
+         System.out.println("오늘 y,n갯수 " + today);
+         
+         return today;
+      }
+      
+      
+      
+      
+      
+      
+      // 게시글 상세보기
+//      @GetMapping("/{idx}") // board/{idx}
+//      public Board boardContent(@PathVariable("idx") int idx) {
+//         Board board = mapper.boardContent(idx);
+//         return board;
+//      }
 //
-//		// 게시글 삭제하기
-//		@DeleteMapping("/{idx}") // board/{idx}
-//		public void boardDelete(@PathVariable("idx") int idx) {
-//			mapper.boardDelete(idx);
-//		}
-//		
-//		// 게시글 수정하기
-//		@PutMapping("/update")
-//		public void boardUpdate(@RequestBody Board board) { // idx, title, content
-//			mapper.boardUpdate(board);
-//		}
-//		
-//		// 게시글 조회수 올리기
-//		@PutMapping("/count/{idx}")
-//		public void boardCount(@PathVariable("idx") int idx) {
-//			mapper.boardCount(idx);
-//		}
-//		
-	
+//      // 게시글 삭제하기
+//      @DeleteMapping("/{idx}") // board/{idx}
+//      public void boardDelete(@PathVariable("idx") int idx) {
+//         mapper.boardDelete(idx);
+//      }
+//      
+//      // 게시글 수정하기
+//      @PutMapping("/update")
+//      public void boardUpdate(@RequestBody Board board) { // idx, title, content
+//         mapper.boardUpdate(board);
+//      }
+//      
+//      // 게시글 조회수 올리기
+//      @PutMapping("/count/{idx}")
+//      public void boardCount(@PathVariable("idx") int idx) {
+//         mapper.boardCount(idx);
+//      }
+//      
+   
 }
